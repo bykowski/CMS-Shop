4a103cffbbc2624067be1a4a36efcd8bdea32c99
Adrian Zawadzki <zawadzkii@tlen.pl>
2020-08-11 20:08:29 +0200

    modify cart

--- src/main/java/com/adrian/cmsshoppingcart/models/Cart.java
+++ src/main/java/com/adrian/cmsshoppingcart/models/Cart.java
@@ -1,8 +1,5 @@
 package com.adrian.cmsshoppingcart.models;
 
-import lombok.Data;
-
-@Data
 public class Cart {
 
     private int id;
@@ -11,11 +8,54 @@ public class Cart {
     private int quantity;
     private String image;
 
+    public Cart() {
+    }
+
     public Cart(int id, String name, String price, int quantity, String image) {
         this.id = id;
         this.name = name;
         this.price = price;
         this.quantity = quantity;
+        this.image = image;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
         this.image = image;
     }
     
